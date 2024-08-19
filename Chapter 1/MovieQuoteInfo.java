a7958025c6af506a731254cff706eb94f2e2200e
florenceamaba <amabaflorence@gmail.com>
2024-08-19 16:23:36 +0800

    Update and rename A to MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo{
+
+public static void main(String[] args){
+
+System.out.println("Qoute: You have to believe in yourself. That's the secret. ”);
+System.out.println("Movie: Kung Fu Panda. ");
+System.out.println("Actor: Po. ");
+System.out.println("Year: 2008 ");
+  
+}
+}
