4a748e1cd8150e494df8c5d25c4e78ca520ba489
florenceamaba <amabaflorence@gmail.com>
2024-09-16 20:51:15 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 2/FirstAddressExcercise.java
@@ -0,0 +1,19 @@
+public class FirstAddressExcercise{
+public static void main(String[] args){
+      
+      System.out.println("First Java application ");
+      displayAddress();   
+   }
+  
+   public static void displayAddress(){
+   
+      System.out.println("XYZ Company ");
+     
+      System.out.println("8900 U.S Hwy 14");
+      
+      System.out.println("Crystal Lake, IL 60014 ");
+      
+      System.out.println("Company: Apple Inc. ");
+
+   }
+ }
